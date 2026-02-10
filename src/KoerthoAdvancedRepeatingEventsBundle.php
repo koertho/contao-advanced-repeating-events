c6f452b77bf2fc704e396f314364af53ee64bef7
Thomas Körner <t.koerner@heimrich-hannot.de>
2026-02-10 19:26:27 +0100

    fix plugins registration

--- src/KoerthoAdvancedRepeatingEventsBundle.php
+++ src/KoerthoAdvancedRepeatingEventsBundle.php
@@ -6,5 +6,10 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class KoerthoAdvancedRepeatingEventsBundle extends AbstractBundle
 {
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
+
 
 }
