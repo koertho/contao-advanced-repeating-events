40d9a835719d1751f7d025dc517e214344b1a21b
Thomas Körner <t.koerner@heimrich-hannot.de>
2026-03-10 09:12:04 +0100

    add ical import listener event

---
+++ src/EventListener/CalendarIcalBundleListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Koertho\AdvancedRepeatingEventsBundle\EventListener;
+
+use Cgoit\ContaoCalendarIcalBundle\Event\AfterImportItemEvent;
+use Koertho\AdvancedRepeatingEventsBundle\Model\CalendarEventsModel;
+use Recurr\Exception\InvalidRRule;
+use Recurr\Rule;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+
+#[AsEventListener]
+class CalendarIcalBundleListener
+{
+    public function __invoke(AfterImportItemEvent $event): void
+    {
+        if (!$event->vevent->getRrule()) {
+            return;
+        }
+
+        $rrule = $event->vevent->createComponent();
+        preg_match('/^RRULE:(.+)$/mi', $rrule, $matches);
+        $rrule = $matches[1] ?? null;
+        if (!is_string($rrule)) {
+            return;
+        }
+        try {
+            // validate rrule
+            new Rule(trim($rrule));
+        } catch (InvalidRRule) {
+        }
+
+        /** @var CalendarEventsModel $model */
+        $model = $event->calendarEventModel;
+
+        $model->recurring = false;
+        $model->areRecurring = true;
+        $model->rrule = $rrule;
+        $model->save();
+    }
+}
